b56edcf874be090832df6f6d4bd195df9a3ad04b
ec2064817 <ec2064817@edinburghcollege.ac.uk>
2022-11-24 17:14:42 +0000

    Initial commit

---
+++ Cooler King/Game1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+
+namespace Cooler_King
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Paddle paddle;
+
+        Rectangle screenSize;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
+            
+        }
+
+        protected override void Initialize()
+        {
+            screenSize = GraphicsDevice.Viewport.Bounds;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //loads the paddle texture
+            paddle = new Paddle(Content.Load <Texture2D>("paddle (2)"), screenSize.Width/2 - 35 , 450);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            //Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            paddle.UpdateMe(screenSize.Width);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            paddle.DrawMe(_spriteBatch);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Cooler King/Paddle.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Cooler_King
+{
+    internal class Paddle
+    {
+        Vector2 pos;
+        public Rectangle Rect;
+        
+
+        KeyboardState kb;
+
+        Texture2D tex;
+        
+
+        //Constructer
+        public Paddle(Texture2D tex, int xPos, int yPos)
+        {
+            this.tex = tex;
+
+            Rect = new Rectangle(xPos, yPos, tex.Width, tex.Height);
+        }
+
+        //Update
+        public void UpdateMe(int screenWidth)
+        {
+            
+            //Setting kb as the keyboard get state function
+            kb = Keyboard.GetState();
+
+            //if the left arrow is pressed the paddle goes left
+            if (kb.IsKeyDown(Keys.Left))
+                Rect.X -= 10;
+
+            //if the right arrow is pressed the paddle goes right
+            if (kb.IsKeyDown(Keys.Right))
+            {
+                Rect.X += 10;
+            }
+                
+
+            //Makes it so the paddle can not leave the screen
+            if (Rect.X > screenWidth - tex.Width)
+            {
+                Rect.X = screenWidth - tex.Width;   
+            }
+
+            if (Rect.X < 0)
+            {
+                Rect.X = 0;
+            }
+        }
+
+
+
+
+        
+        
+
+        public void DrawMe(SpriteBatch sb)
+        {
+            //draws the paddle at the predefined location with the texture
+            sb.Draw(tex, Rect, Color.White);
+        }
+
+        
+        
+    }
+}
